96030e56352c24363f6983c49d3dd7caf0ea04b5
Simon Briesenick <113053839+simonwb98@users.noreply.github.com>
2024-09-19 16:36:16 -0400

    added some comments

--- src/main.py
+++ src/main.py
@@ -28,22 +28,34 @@ def main():
 # Approaching a more phyisically meaningful system - Reducing the complexity by adding one monomer at a time
 
 def initialize_dimer(lattice, monomer_params):
-    x_center, y_center = (lattice.width//2, lattice.width//2)
+    '''
+    Here, a dimer is initialized as two coupled monomers in the centre of the lattice. The second monomer position is chosen at random 
+    from the available next_nearest_neighbour positions.
+    
+    Returns the two monomer objects.
+    '''
+    x_center, y_center = (lattice.width//2, lattice.width//2) # find centre
     monomer_1 = Monomer(*monomer_params)
-    monomer_1.set_position(x_center, y_center)
+    monomer_1.set_position(x_center, y_center) # set position of monomer 1
     if lattice.rotational_symmetry == 6:
         orientation_1 = monomer_1.get_orientation()
-        next_nearest_neighbours = lattice.get_next_nearest_neighbours(x_center, y_center, orientation_1)
+        next_nearest_neighbours = lattice.get_next_nearest_neighbours(x_center, y_center, orientation_1) # get available next_nearest_neighbours positions
         monomer_2 = Monomer(*monomer_params)
         x_2, y_2 = random.choice(next_nearest_neighbours)
         monomer_2.set_position(x_2, y_2)
-        monomer_2.set_orientation(orientation_1 + 180 if orientation_1 == 0 else 0)
+        monomer_2.set_orientation(orientation_1 + 180 if orientation_1 == 0 else 0) # make sure it has opposite orientation to monomer 1
         monomer_1.couple_with(monomer_2)
         lattice.place_monomer(monomer_1, x_center, y_center)
         lattice.place_monomer(monomer_2, x_2, y_2)
     return (monomer_1, monomer_2)
 
 def introduce_new_monomer(lattice, new_monomer, monomers, max_steps=1e5):
+    '''
+    Introduces a new monomer on the lattice and executes the Monomer.action() method iteratively until 
+    monomer is coupled (at which point it is defined to not move anymore) or until max_steps has been reached. 
+    In the latter case, the monomer is removed from the lattice. This was done to avoid having several islands growing at the same time. 
+    We might want to relax this condition at some point.
+    '''
     steps = 0
     while steps < max_steps:
         new_monomer.action(lattice)
@@ -58,12 +70,17 @@ def introduce_new_monomer(lattice, new_monomer, monomers, max_steps=1e5):
         print(f"Monomer failed to couple after {max_steps} steps. Initializing new monomer...")
 
 def slow_growth_simulation(lattice, monomer_params, total_monomers, max_steps=1e5):
+    '''
+    What I call slow_growth here is what I had explained in our meeting, where the density of monomers is so low that 
+    it is physically accurate to model only a single monomer at a time until it coupled to the growing island. Only after 
+    the monomer has coupled is the next one introduced.
+    '''
     monomer_1, monomer_2 = initialize_dimer(lattice, monomer_params)
     monomers = [monomer_1, monomer_2]
 
     for i in range(2, total_monomers):
         new_monomer = Monomer(*monomer_params)
-        lattice.randomly_place_monomers([new_monomer])
+        lattice.randomly_place_monomers([new_monomer]) # initialize monomer with random position (note that this can also be inside the island on an unoccupied site)
         introduce_new_monomer(lattice, new_monomer, monomers, max_steps)
         
 
@@ -71,7 +88,7 @@ def slow_growth_simulation(lattice, monomer_params, total_monomers, max_steps=1e
 
     neighbour_freq, radius, radius_of_gyration = analyze_structure(lattice, monomers)
 
-    plot_analysis_results(neighbour_freq, radius, lattice, monomers)
+    plot_analysis_results(neighbour_freq, radius, lattice, monomers) # some preliminary analysis of the resulting structure
 
 if __name__ == "__main__":
     main()
